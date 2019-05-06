94d0a61318e41aebd600fff77d035559c3c5c70b
Isaque Alves <isaque.mace@gmail.com>
2019-05-06 20:25:56 -0300

    Finalizado o método de exibição da previsão do tempo ao buscar por uma cidade

--- app/src/main/java/br/com/bossini/usjt_ads3anmca_weatherforecast/MainActivity.java
+++ app/src/main/java/br/com/bossini/usjt_ads3anmca_weatherforecast/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.bossini.usjt_ads3anmca_weatherforecast;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,10 +38,14 @@ public class MainActivity extends AppCompatActivity {
     private WeatherAdapter adapter;
     private List<Weather> previsoes;
     private EditText locationEditText;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestQueue = Volley.newRequestQueue(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -54,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                         getString(R.string.api_key)
                 );
 
-                obtemPrevisoesV3(endereco);
+                obtemPrevisoesV5(cidade);
             }
         });
     }
@@ -133,10 +149,125 @@ public class MainActivity extends AppCompatActivity {
                 String json = resultado.toString();
                 runOnUiThread(() -> {
                     Toast.makeText(this, resultado.toString(), Toast.LENGTH_SHORT).show();
+                    lidaComJSON(resultado.toString());
                 });
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    class ObtemPrevisoes extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... enderecos) {
+            try {
+                String endereco = getString(
+                        R.string.web_service_url,
+                        enderecos[0],
+                        getString(R.string.api_key)
+                );
+                URL url = new URL(endereco);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                InputStream is = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                StringBuilder resultado = new StringBuilder("");
+                String aux = null;
+                while ((aux = reader.readLine()) != null)
+                    resultado.append(aux);
+                return resultado.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            lidaComJSON(resultado);
+        }
+    }
+
+    public void obtemPrevisoesV4(String endereco) {
+        new ObtemPrevisoes().execute(endereco);
+    }
+
+    public void obtemPrevisoesV5 (String cidade){
+        String url = getString(
+                R.string.web_service_url,
+                cidade,
+                getString(R.string.api_key)
+        );
+        JsonObjectRequest req = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                (response)->{
+                    //aqui vamos tratar o json, cujo nome é response nesse caso
+                    previsoes.clear();
+                    try{
+                        JSONArray list = response.getJSONArray("list");
+                        for (int i = 0; i < list.length(); i++){
+                            JSONObject day = list.getJSONObject(i);
+                            JSONObject main = day.getJSONObject("main");
+                            JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
+                            previsoes.add (new Weather(day.getLong("dt"), main.getDouble("temp_min"),
+                                    main.getDouble("temp_max"), main.getDouble ("humidity"),
+                                    weather.getString("description"),weather.getString("icon")));
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                    catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                },
+                (error)->{
+                    Toast.makeText(
+                            MainActivity.this,
+                            getString(R.string.connect_error) + ": " + error.getLocalizedMessage(),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+        );
+        requestQueue.add(req);
+    }
+
+    private void dismissKeyboard (View view){
+        InputMethodManager imm = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+    }
+
+    public void lidaComJSON(String resultado) {
+        previsoes.clear();
+        try {
+            JSONObject json = new JSONObject(resultado);
+            JSONArray list = json.getJSONArray("list");
+            for (int i = 0; i < list.length(); i++){
+                JSONObject previsaoDaVez = list.getJSONObject(i);
+                long dt = previsaoDaVez.getLong("dt");
+                JSONObject main = previsaoDaVez.getJSONObject("main");
+                double temp_min = main.getDouble("temp_min");
+                double temp_max = main.getDouble("temp_max");
+                double humidity = main.getDouble("humidity");
+                JSONArray weather = previsaoDaVez.getJSONArray("weather");
+                JSONObject unico = weather.getJSONObject(0);
+                String description = unico.getString("description");
+                String icon = unico.getString("icon");
+                Weather w = new Weather(
+                        dt,
+                        temp_min,
+                        temp_max,
+                        humidity,
+                        description,
+                        icon
+                );
+                previsoes.add(w);
+            }
+            adapter.notifyDataSetChanged();
+            dismissKeyboard(weatherRecyclerView);
+        } catch (JSONException e) {
+            Toast.makeText(this, getString(R.string.read_error), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
